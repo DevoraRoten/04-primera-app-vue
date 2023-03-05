bae5347dc60d5fbdd5863bc421c07eda7fd9d3d2
Devora Rohten <d.rohten01@gamil.com>
2023-03-05 18:29:04 -0300

    watched question

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
 <div>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <!--<MiPrimerComponente title="Entraron"/>-->
-  <MiPrimerComponente />
+ <!-- <img alt="Vue logo" src="./assets/logo.png">-->
+  <!--<MiPrimerComponente title="Entraron"/>
+  <MiPrimerComponente />-->
+  <indecision-component-vue/>
 </div>
 
 </template>
 
 <script>
+import IndecisionComponentVue from './components/IndecisionComponent.vue'
+
 //import HelloWorld from './components/HelloWorld.vue'
-import MiPrimerComponente from './components/MiPrimerComponente.vue'
+//import MiPrimerComponente from './components/MiPrimerComponente.vue'
+//import IndecisionComponentVue from './components/IndecisionComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    MiPrimerComponente
+    //MiPrimerComponente,
+    IndecisionComponentVue
   }
 }
 </script>
---
+++ src/components/IndecisionComponent.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+ <img alt="Vue logo" src="../assets/logo.png">
+   <div class="bg-dark"></div>
+
+   <div class="indecision-conainer">
+      <input type="text" v-model="question" placeholder="Hazme una pregunta">
+      <p>Recuerda  terminar con un signo de interrogacion (?)</p>
+   </div>
+
+   <div>
+      <h2>{{question}}</h2>
+      <h1>resp</h1>
+   </div>
+
+</div>
+  
+
+</template>
+
+<script>
+export default {
+   data(){
+      return{
+         question: null
+      }
+      
+   },
+   watch: {
+      question(value){
+         if(!value.includes('?')) return
+      }
+
+   }
+
+}
+</script>
+
+<style scoped>
+
+   
+
+</style>
