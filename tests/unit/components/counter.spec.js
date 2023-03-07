1f89a627f8ed865acb7d7e9c1207d49f488ba878
Devora Rohten <d.rohten01@gamil.com>
2023-03-07 17:56:29 -0300

    evento clic en incrementar y decrementar

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -2,21 +2,27 @@ import { shallowMount } from "@vue/test-utils";
 import Counter  from "@/components/Counter";
 
 describe(' Counter.vue', ()=>{
+    let wrapper;
+
+    beforeEach(()=>{
+        wrapper= shallowMount(Counter)
+    })
+
     test('debe de hacer match con el snapshot', ()=>{
         
-        const wrapper = shallowMount(Counter)
+      //  const wrapper = shallowMount(Counter)
         expect(wrapper.html()).toMatchSnapshot()
 
     })
     test('h2 debe tener el valor "Counter"',()=>{
-        const wrapper = shallowMount(Counter)
+      //  const wrapper = shallowMount(Counter)
         expect( wrapper.find('h2').exists()).toBeTruthy()
 
         const h2Value = wrapper.find('h2').text()
         expect(h2Value).toBe('Counter')
     })
     test('valor por defecto del counter debe ser 15',()=>{
-        const wrapper = shallowMount(Counter)
+       // const wrapper = shallowMount(Counter)
        /* const h3Tags = wrapper.findAll('h3')
         console.log(h3Tags[1])
         expect(h3Tags[1].text()).toBe('15')*/
@@ -24,11 +30,19 @@ describe(' Counter.vue', ()=>{
         expect(h3Tag).toBe('15')
     })
 
-    test('debe incrementar 1 el valor del contador',async()=>{
-        const wrapper = shallowMount(Counter)
-        const incrementarBtn = wrapper.find('button')
+    test('debe incrementar 1 y decrementar 2 el valor del contador',async()=>{
+        //const wrapper = shallowMount(Counter)
+        const [incrementarBtn, decrementarBtn] = wrapper.findAll('button')
         await incrementarBtn.trigger('click')
         const h3Tag=wrapper.find('[data-testid="counter"]').text()
         expect(h3Tag).toBe('16')
+
+        //boton de decrementar
+
+        await decrementarBtn.trigger('click')
+        await decrementarBtn.trigger('click')
+        const h3TagD=wrapper.find('[data-testid="counter"]').text()
+        expect(h3TagD).toBe('14')
+
     })
 })
